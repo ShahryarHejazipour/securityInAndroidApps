d59af5f1e73a720e13dff31314023c81484888fe
Shahryar Hejazipour <shahryar.hejazipour@outlook.com>
2021-10-08 00:21:42 +0330

    implementing Encrypting the Strings with BuildConfig method and with NDK method for more Security

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,11 @@ Java_com_tispunshahryar960103_securityman_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++\n implementing the simple NDK is successful";
     return env->NewStringUTF(hello.c_str());
 }
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_tispunshahryar960103_securityman_api_ApiClient_getUrl(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string mUrl = "http://api.androidsupport.ir/";
+    return env->NewStringUTF(mUrl.c_str());
+}
--- app/src/main/java/com/tispunshahryar960103/securityman/api/ApiClient.java
+++ app/src/main/java/com/tispunshahryar960103/securityman/api/ApiClient.java
@@ -1,12 +1,26 @@
 package com.tispunshahryar960103.securityman.api;
 
+import android.util.Base64;
+
+import com.tispunshahryar960103.securityman.BuildConfig;
+
+import java.io.UnsupportedEncodingException;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
+    static {
+        System.loadLibrary("ndksample");
+    }
 
-    public static final String BASE_URL="http://api.androidsupport.ir/";
+
+    //public static final String BASE_URL="http://api.androidsupport.ir/";
+   //public static final String BASE_URL= BuildConfig.serverUrl;
+   //public static final String BASE_URL= getUrl();
+   public static final String BASE_URL= getSecureBaseUrl();
+
     private static Retrofit retrofit=null;
 
     public static Retrofit getClient(){
@@ -26,6 +40,33 @@ public class ApiClient {
 
 
     }
+
+    public static native String getUrl();
+
+
+    /*
+    برای بهینه تر کردن امنیت رشته های تابع getUrl از تابع زیر استفاده می شود که
+    می گوید مقدار mUrl را گرفته و decode کند و مقدار آن را برگرداند. در غیر اینصورت یک مقدار نادرست
+    مانند "https://gdgsdhguk.com/" را برگرداند تا اگر کسی فایل apk را decompile کرد نتواند به رشته های ما
+    دشترسی داشته باشد.
+     */
+    public static String getSecureBaseUrl(){
+
+        String mUrl=getUrl();
+
+        try {
+            String text = new String(Base64.decode(mUrl,Base64.DEFAULT),"UTF-8");
+            return text;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        mUrl="https://gdgsdhguk.com/";
+        return mUrl;
+
+    }
+
+
 
 
 
